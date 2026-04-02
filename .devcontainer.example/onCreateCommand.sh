826518d8786c05405e4e17f9f5eb701efd9b9da6
Yuchan Lee <lasuillard@gmail.com>
2026-04-02 09:31:43 +0000

    chore: update cargo-binstall installation mechanism

--- .devcontainer.example/onCreateCommand.sh
+++ .devcontainer.example/onCreateCommand.sh
@@ -2,10 +2,9 @@
 
 ARCH="$(dpkg --print-architecture)"
 
-CARGO_BINSTALL_VERSION="1.12.5"
-
-curl -fsSL "https://github.com/cargo-bins/cargo-binstall/releases/download/v${CARGO_BINSTALL_VERSION}/cargo-binstall-$(rustc -vV | sed -n 's|host: ||p').tgz" \
-    | tar --extract --gzip --directory "${CARGO_HOME}/bin"
+# cargo-binstall
+curl --proto '=https' --tlsv1.2 --silent --fail --show-error --location https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
+    | bash
 
 # Download dev tools binaries
 cargo binstall -y --log-level debug \
